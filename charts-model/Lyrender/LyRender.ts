a8c538493623675f6bc91354c83cd3706b54d702
luoyu11 <luoyu11@meituan.com>
2019-07-25 19:43:26 +0800

    逻辑

--- charts-model/Lyrender/LyRender.ts
+++ charts-model/Lyrender/LyRender.ts
@@ -1,8 +1,14 @@
 import LyElement from './LyElements/LyElement'
+import Stage from './Stage'
+import Painter from './Painter'
 
 class LyRender {
-
+  stage: Stage
+  painter: Painter
   constructor (dom: string | HTMLElement) {
+      let stage = new Stage()
+      this.stage = stage
+      this.painter = new Painter(dom, stage)
   }
 
   add () {
---
+++ charts-model/Lyrender/Painter.ts
@@ -0,0 +1,9 @@
+import Stage from './Stage'
+
+class Painter {
+    constructor (dom: String | HTMLElement, stage: Stage) {
+      console.log('Painter')
+    }
+  }
+  
+  export default Painter
