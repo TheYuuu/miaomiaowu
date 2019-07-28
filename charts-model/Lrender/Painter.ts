260bd570a53b12f378eb6f49f5e9f3e5572580c6
luoyu11 <luoyu11@meituan.com>
2019-07-28 15:59:34 +0800

    v1 完成

--- charts-model/Lrender/Painter.ts
+++ charts-model/Lrender/Painter.ts
@@ -29,6 +29,7 @@ class Painter {
     }
 
     render () {
+      // 渲染前清空图像
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       let xelements = this.stage.getAll()
       for (let i = 0; i < xelements.length; i += 1) {
