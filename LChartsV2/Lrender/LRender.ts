f84f175ecb367f3d1039b99e258c476025e05c69
luoyu11 <luoyu11@meituan.com>
2019-07-30 14:19:46 +0800

    v1v2

---
+++ LChartsV2/Lrender/LRender.ts
@@ -0,0 +1,29 @@
+import LElement from './LElements/LElement'
+import Stage from './Stage'
+import Painter from './Painter'
+
+class LRender {
+  stage: Stage
+  painter: Painter
+  constructor (dom: string | HTMLElement) {
+      let stage = new Stage()
+      this.stage = stage
+      this.painter = new Painter(dom, stage)
+  }
+
+  add (...lelements: LElement[]) {
+    this.stage.add(...lelements)
+    this.render()
+  }
+
+  delete (xel: LElement) {
+    this.stage.delete(xel)
+    this.render()
+  }
+
+  render () {
+    this.painter.render()
+  }
+}
+
+export default LRender
---
+++ LChartsV2/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <div id="canvas"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import * as Lrender from '../Lrender'
+
+@Component({
+  name: 'app',
+  mounted () {
+    var lr = Lrender.init('canvas')
+
+    let circle = new Lrender.Circle({
+      shape: {
+        cx: 60,
+        cy: 60,
+        r: 20
+      },
+      style: {
+        fill: '#00f'
+      }
+    })
+
+    let circle1 = new Lrender.Circle({
+      shape: {
+        cx: 120,
+        cy: 60,
+        r: 20
+      },
+      style: {
+        fill: 'red'
+      }
+    })
+
+    let circle2 = new Lrender.Circle({
+      shape: {
+        cx: 180,
+        cy: 60,
+        r: 20
+      },
+      style: {
+        fill: 'yellow'
+      }
+    })
+
+    let rect = new Lrender.Rect({
+      shape: {
+        x: 180,
+        y: 60,
+        width: 20,
+        height: 20
+      },
+      style: {
+        fill: 'green'
+      }
+    })
+
+    lr.add(circle, circle1, circle2, rect)
+    lr.delete(circle2)
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#canvas {
+  width: 500px;
+  height: 500px;
+  border: 1px solid lavender;
+}
+</style>
