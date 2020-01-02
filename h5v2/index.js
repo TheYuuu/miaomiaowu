df8a0d6dfb95e31693994fe45c3816bf24d041b4
luoyu <luoyu.creative@icloud.com>
2020-01-02 21:24:29 +0800

    feat[h5v2] scrollmagic实验

--- h5v2/index.js
+++ h5v2/index.js
@@ -3,46 +3,76 @@ let tl = new TimelineMax({
   repeatDelay: 0
 });
 
-// 背景缩上去
-tl.add(TweenMax.to('.background_block', 1, {
-  'height': '40%',
-  'top': '-20%',
-  'width': '110%',
-  'border-radius': '50%',
-}), TweenMax.to('.bu_logo', 1, {
-  'width': '6rem',
-  'top': '10%'
-}), TweenMax.fromTo('.bu_title', 1, {
-  'top': '110%',
-  'color': '#F2341A',
-}, {
-  'top': '99%',
-  'color': '#F2341A',
-}))
+tl.add(background_block())
+  .add(brand())
+  .add(page1Content())
 
-let text_g = [];
-for (let i = 16; i <= 126; i+=2) {
-  text_g.push('#path' + i);
+function background_block() {
+  var tl = new TimelineLite();
+  // 背景缩上去
+  tl.add(TweenMax.to('.background_block', 1, {
+    'height': '40%',
+    'top': '-20%',
+    'width': '110%',
+    'border-radius': '50%',
+  }), TweenMax.to('.bu_logo', 1, {
+    'width': '6rem',
+    'top': '10%'
+  }), TweenMax.fromTo('.bu_title', 1, {
+    'top': '110%',
+    'color': '#F2341A',
+  }, {
+    'top': '99%',
+    'color': '#F2341A',
+  }))
+  return tl;
 }
 
-tl.add(TweenMax.fromTo(text_g, 1, {
-  'fill': '#rgba(255, 255, 255, 0)',
-  'stroke': 'rgba(255, 255, 255, 0)',
-  'stroke-dashoffset': 200,
-  'stroke-dasharray': 100
-}, {
-  'fill': 'rgba(255, 255, 255, 0)',
-  'stroke': 'white',
-  'stroke-dashoffset': 0
-})).add(TweenMax.to(text_g, 0.5, {
-  'fill': 'white',
-  'stroke': 'none',
-}))
+function brand() {
+  var tl = new TimelineLite();
+  let text_g = [];
+  for (let i = 16; i <= 126; i += 2) {
+    text_g.push('#path' + i);
+  }
 
+  tl.add(TweenMax.fromTo(text_g, 1, {
+    'fill': '#rgba(255, 255, 255, 0)',
+    'stroke': 'rgba(255, 255, 255, 0)',
+    'stroke-dashoffset': 200,
+    'stroke-dasharray': 100
+  }, {
+    'fill': 'rgba(255, 255, 255, 0)',
+    'stroke': 'white',
+    'stroke-dashoffset': 0
+  })).add(TweenMax.to(text_g, 0.5, {
+    'fill': 'white',
+    'stroke': 'none',
+  }))
+  return tl;
+}
 
-tl.add(TweenMax.fromTo('.title-list-item', 1, {
-  x: -50
-}, {
-  x: 0,
-  opacity: 1
-}))
+function page1Content() {
+  var tl = new TimelineLite();
+  tl.from('.title-list', 0.4, {
+      scale: 0,
+      ease: Power1.easeInOut
+    })
+    .from('.title-list', 0.3, {
+      rotation: 90,
+      ease: Power1.easeInOut
+    }, 0)
+    .staggerFrom(".title-list-item", 1.1, {
+      y: -50,
+      opacity: 0,
+      ease: Elastic.easeOut
+    }, 0.5)
+    .addLabel("out", "+=1")
+    .to('.tudou', 0.3 , {
+      rotationY:180, 
+      transformOrigin:"50% 0%", 
+      ease: Cubic.easeOut,
+      yoyo: true,
+      repeat: 1
+    })
+  return tl;
+}
