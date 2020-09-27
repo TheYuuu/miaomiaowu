9d83e172243bbc1ded3b8300348e3ed8ddb51843
TheYuuu <luoyu.creative@icloud.com>
2020-09-27 15:43:59 +0800

    辅助轴

--- three-l/src/components/drawer.js
+++ three-l/src/components/drawer.js
@@ -14,15 +14,16 @@ export class Drawer {
     this.addLight();
     this.addGalaxy();
     this.addControls();
+    this.showAxes();
     this.render();
   }
 
   init(dom) {
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.camera.position.x = 20;
-    this.camera.position.y = 20;
-    this.camera.position.z = 20;
+    this.camera.position.x = 10;
+    this.camera.position.y = 15;
+    this.camera.position.z = 30;
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     this.webGLRenderer = new THREE.WebGLRenderer();
@@ -40,11 +41,23 @@ export class Drawer {
     this.scene.add(sphere);
   }
 
-  addControls() {
-    this.orbitControls = new OrbitControls(this.camera, this.webGLRenderer.domElement);
-    this.orbitControls.autoRotate = false;
+  createMesh(geom) {
+    const textureLoader = this.textureLoader;
+    const map = textureLoader.load(require('../assets/textures/planets/Earth.png'));
+    const bumpMap = textureLoader.load(require('../assets/textures/planets/earthbump1k.jpg'));
+    const specularMap = textureLoader.load(require('../assets/textures/planets/EarthSpec.png'));
 
-    this.clock = new THREE.Clock();
+    const material = new THREE.MeshPhongMaterial({
+      bumpScale: 0.05,
+      specular: new THREE.Color('grey'),
+      shininess: 10
+    });
+
+    material.map = map;
+    material.bumpMap = bumpMap;
+    material.specularMap = specularMap;
+
+    return new THREE.Mesh(geom, material);;
   }
 
   addLight() {
@@ -69,23 +82,16 @@ export class Drawer {
     this.scene.add(galaxy);
   }
 
-  createMesh(geom) {
-    const textureLoader = this.textureLoader;
-    const map = textureLoader.load(require('../assets/textures/planets/Earth.png'));
-    const bumpMap = textureLoader.load(require('../assets/textures/planets/earthbump1k.jpg'));
-    const specularMap = textureLoader.load(require('../assets/textures/planets/EarthSpec.png'));
+  addControls() {
+    this.orbitControls = new OrbitControls(this.camera, this.webGLRenderer.domElement);
+    this.orbitControls.autoRotate = false;
 
-    const material = new THREE.MeshPhongMaterial({
-      bumpScale: 0.05,
-      specular: new THREE.Color('grey'),
-      shininess: 10
-    });
+    this.clock = new THREE.Clock();
+  }
 
-    material.map = map;
-    material.bumpMap = bumpMap;
-    material.specularMap = specularMap;
-
-    return new THREE.Mesh(geom, material);;
+  showAxes() {
+    var axes = new THREE.AxisHelper(300);
+    this.scene.add(axes);
   }
 
   render() {
