be118e0b641d15ff16111046c59eca79958431e0
the yu <luoyu.creative@icloud.com>
2020-10-23 23:37:33 +0800

    nav

--- BTV/b-tv/src/App.vue
+++ BTV/b-tv/src/App.vue
@@ -2,24 +2,9 @@
 <div v-on:keydown="keyDown($event)" class="h-full">
   <el-container class="h-full">
     <el-header>
-      <div v-for="(item) in [padArr]" :key="JSON.stringify(item)" class="flex_con pad_con">
-        <el-card v-for="(mm) in item" :key="mm.name" class="box-card chooseAble" tabindex="0" @keyup.enter.native="changePad(mm)">
-          {{ mm.name }}
-        </el-card>
-      </div>
-      <div v-for="(item) in [navArr]" :key="JSON.stringify(item)" class="flex_con nan_con">
-        <el-card v-for="(mm) in item" :key="mm.name" class="box-card chooseAble" tabindex="0" @focus.native="changeTab(mm)">
-          {{ mm.name }}
-        </el-card>
-      </div>
-      <div v-for="(item) in [cat2Arr]" :key="JSON.stringify(item)" class="flex_con subNan_con">
-        <el-card v-for="(mm) in item" :key="mm.name" class="box-card chooseAble chooseAble_sub" tabindex="0" @focus.native="changeSubTab($event, mm)">
-          {{ mm.name }}
-        </el-card>
-      </div>
+      <nav-con />
     </el-header>
     <el-main>
-      <!-- <nav-con :list="navArr" ref="navCon"></nav-con> -->
       <block />
     </el-main>
   </el-container>
@@ -35,6 +20,7 @@ import {
 
 import videoDrawer from './components/videoDrawer';
 import block from './components/block';
+import navCon from './components/navCon';
 
 import {
   elControlStore
@@ -44,33 +30,17 @@ import {
   name: 'App',
   components: {
     videoDrawer,
-    block
+    block,
+    navCon
   }
 })
 export default class extends Vue {
-  @elControlStore.State('padArr') padArr;
-  @elControlStore.State('navArr') navArr;
   @elControlStore.Getter('allArr') getAllArr;
-
-  @elControlStore.Action('setCat1RidAction') setCat1RidAction;
-  @elControlStore.Action('setCat2RidAction') setCat2RidAction;
-  @elControlStore.Action('setCat2ArrAction') setCat2ArrAction;
-
-  @elControlStore.Getter('getCat2Arr') getCat2Arr;
-
-  @elControlStore.Action('getWatchBlockArrByRid') getWatchBlockArrByRid;
 
   get allArr() {
     return this.getAllArr;
   }
-
-  get cat2Arr() {
-    return this.getCat2Arr;
-  }
-
   mounted() {
-    this.changeTab(this.navArr[0]);
-
     this.$nextTick(() => {
       this.getChooseAble();
       this.inputs[0].focus();
@@ -125,26 +95,6 @@ export default class extends Vue {
           break;
       }
     });
-  }
-
-  changeTab(item) {
-    this.setCat1RidAction(item.rid);
-
-    const sub = item.sub || [];
-    if (sub[0]) {
-      this.setCat2RidAction((sub[0].rid));
-    }
-
-    this.setCat2ArrAction(item.sub || []);
-    this.getWatchBlockArrByRid();
-  }
-
-  changeSubTab(item) {
-    // console.log(evt)
-  }
-
-  changePad(item) {
-    console.log(123, item)
   }
 }
 </script>
