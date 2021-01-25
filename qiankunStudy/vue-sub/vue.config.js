49cac7596ad13d6fc7d4cc81ad2f70b42fd35360
TheYuuu <luoyu.creative@icloud.com>
2021-01-25 20:00:11 +0800

    加载图片

--- qiankunStudy/vue-sub/vue.config.js
+++ qiankunStudy/vue-sub/vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
     }
   },
   devServer: {
-    port: 7777, // 在.env中VUE_APP_PORT=7788，与父应用的配置一致
+    port: process.env.VUE_APP_PORT, // 在.env中VUE_APP_PORT=7788，与父应用的配置一致
     headers: {
       'Access-Control-Allow-Origin': '*' // 主应用获取子应用时跨域响应头
     }
