e1f7ff3a1601d2589e42cea03276220455a4b80c
TheYuuu <luoyu.creative@icloud.com>
2021-03-22 19:13:42 +0800

    vue init

--- unUyoGraphy/build-scripts/webpack.config.js
+++ unUyoGraphy/build-scripts/webpack.config.js
@@ -5,16 +5,37 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   entry: path.resolve(__dirname, '../src/index.ts'),
   output: {
-    path: path.join(__dirname, '../packages'),
+    path: path.join(__dirname, '../dist'),
+    chunkFilename: '[id].js',
     filename: 'bundle.js'
   },
   plugins: [],
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          'vue-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
--- unUyoGraphy/build-scripts/webpack.demo.js
+++ unUyoGraphy/build-scripts/webpack.demo.js
@@ -1,21 +1,53 @@
 const path = require('path');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const { VueLoaderPlugin } = require('vue-loader/dist/index');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: path.resolve(__dirname, '../example/enrty.ts'),
   output: {
-    path: path.join(__dirname, '../packages'),
-    filename: 'bundle.js'
+    path: path.resolve(process.cwd(), './publish'),
+    publicPath: process.env.PUBLIC_URL || '',
+    filename: '[name].[hash:7].js',
+    chunkFilename: isProd ? '[name].[hash:7].js' : '[name].js'
   },
-  plugins: [],
+  devServer: {
+    host: '0.0.0.0',
+    port: 8085,
+    publicPath: '/',
+    hot: true,
+    open: true
+  },
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          'vue-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
@@ -38,6 +70,7 @@ module.exports = {
       template: path.resolve(__dirname, '../example/index.html'),
       filename: 'index.html',
       title: 'unUyo Graphy'
-    })
+    }),
+    new VueLoaderPlugin()
   ]
 }
